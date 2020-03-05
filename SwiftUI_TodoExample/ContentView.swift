44e3e52d7decf19836009946c7645eaf0488e07b
jnphgs <mails@jnphgs.net>
2020-03-05 14:26:15 +0900

    Initial Commit

---
+++ SwiftUI_TodoExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_TodoExample
+//
+//  Created by higashi on 2020/03/05.
+//  Copyright © 2020 jnphgs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
